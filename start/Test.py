8131f3fb759c320c2be162e63be8f6b47c8c5a53
linlianyu <18822913165@163.com>
2019-04-04 00:32:42 +0800

    linlianyu

--- start/Test.py
+++ start/Test.py
@@ -3,8 +3,4 @@
 # @Author   : linlianyu
 # @File     : TestReader.py
 import time
-print("Loading", end='')
-for i in range(6):
-    print(".", end='', flush=True)
-    time.sleep(0.2)
-
+words = input('输入')
